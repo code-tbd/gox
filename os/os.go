bf8a780bc2d1fc809404e0936ef88e329fd72c5d
code-tbd <ukmukk@163.com>
2022-10-18 18:33:24 +0800

    os

--- os/os.go
+++ os/os.go
@@ -1,1 +1,22 @@
 package os
+
+import (
+	"os"
+	"path/filepath"
+)
+
+// Create x 传入完整绝对路径，不用担心路径不存在
+func Create(path string) (*os.File, error) {
+	dir := filepath.Dir(path)
+	err := os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		return nil, err
+	}
+
+	f, err := os.Create(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return f, nil
+}
